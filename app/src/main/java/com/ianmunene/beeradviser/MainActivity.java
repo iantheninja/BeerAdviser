91f63037744fb05e5f986ef253f086bdf43f74e2
Ian Munene <munene.ian@live.com>
2017-11-27 15:46:51 +0300

    first version, displays selected spinner value in text view

--- app/src/main/java/com/ianmunene/beeradviser/MainActivity.java
+++ app/src/main/java/com/ianmunene/beeradviser/MainActivity.java
@@ -1,13 +1,23 @@
 package com.ianmunene.beeradviser;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void findBeer(View view) {
+        TextView brands = findViewById(R.id.brands);
+        Spinner color = findViewById(R.id.color);
+        String selectedColor = String.valueOf(color.getSelectedItem());
+        brands.setText(selectedColor);
     }
 }
